ca3ebe0ecd194aa4835bc08f72139a0bf0cf3a6e
i960107 <i960107@naver.com>
2023-11-22 16:32:03 +0900

    feat: 서비스 구현 - 서비스 레이어의 메소드는 비지니스 용어를 사용. 레포지토리와 구분됨

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,46 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        // 같은 이름 중복 체크
+        validateDuplicateMember(member);
+        return memberRepository.save(member).getId();
+    }
+
+    // TODO: 2023/11/22 왜 에러 처리 없는데 컴파일 에러 안 생기지?
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 특정 회원 조회
+     */
+    public Optional<Member> findMember(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
