280ab40cc30c40ae998253201dbd7a34e2c7d7ed
i960107 <i960107@naver.com>
2023-11-24 16:36:41 +0900

    feat: jdbcMemberRepository 추가
    - java 코드 방식으로 기존 코드 수정 없이 쉽게 MemoryMemberRepository -> JdbcmemberRepository로 수정

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,8 +1,9 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,13 +11,15 @@ import org.springframework.context.annotation.Configuration;
 public class SpringConfig {
 
     @Bean
-    public MemberService memberService() {
-        // TODO: 2023/11/23 함수를 호출하면 application context에 등록된 객체를 가져옴? 
-        return new MemberService(memberRepository());
+    public MemberService memberService(DataSource dataSource) {
+        // TODO: 2023/11/23 함수를 호출하면 application context에 등록된 객체를 가져옴?
+        // 따로 객체 생성되는 것 아님?
+        return new MemberService(memberRepository(dataSource));
     }
 
     @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+    public MemberRepository memberRepository(DataSource dataSource) {
+        //여기만 바꾸어주면 repository 교체됨.
+        return new JdbcMemberRepository(dataSource);
     }
 }
